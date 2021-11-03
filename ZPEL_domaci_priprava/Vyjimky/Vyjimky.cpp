8ae7a43bb0f4a3e02d9b119055dd7de5a3de3e91
Lapi <vojtechlapunik@gmail.com>
2021-11-03 10:11:56 +0100

    Pridani vyjimek do domaci pripravy

--- ZPEL_domaci_priprava/Vyjimky/Vyjimky.cpp
+++ ZPEL_domaci_priprava/Vyjimky/Vyjimky.cpp
@@ -2,7 +2,48 @@
 
 using namespace std;
 
-int main()
+int main() 
 {
-// Dělení nulou
+    int citatel;
+    int jmenovatel;
+    double vysledek;
+
+    cout << "Zadej citatel: ";
+    cin >> citatel;
+    cout << "Zadej jmenovatel: ";
+    cin >> jmenovatel;
+
+    //vysledek = citatel / jmenovatel;
+    //cout << "Vysledek deleni: " << vysledek;
+    
+    //*
+
+    try 
+    {
+        if (jmenovatel == 0) 
+        {
+            //throw "";
+            throw "Nelze delit nulou";
+        }
+
+        vysledek = citatel / jmenovatel;
+
+        if (vysledek > 5) 
+        {
+            //throw "";
+            throw "Vysledek je vetsi nez 5!";
+        }
+
+        cout << "Vysledek deleni: " << vysledek;
+
+    }
+    //catch (...)
+    catch(const char* text)
+    {
+        //cout << "Vyjimka";
+        cout << text;
+    }
+
+    //*/
+
 }
