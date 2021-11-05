2f4bd900560761868bf36491bba2f1821a637c79
Lapi <vojtechlapunik@gmail.com>
2021-11-05 17:51:03 +0100

    Domaci priprava na dalsi cviceni

--- ZPEL_domaci_priprava/Kalk_test/Kalk_test.cpp
+++ ZPEL_domaci_priprava/Kalk_test/Kalk_test.cpp
@@ -1,1 +1,84 @@
-﻿
+﻿// Kalkulačka.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+label1:
+    cout << "Ahoj ja jsem kalkulacka, zadej prvni hodnotu: \n";
+    float cislo1;
+    cin >> cislo1;
+
+
+
+    cout << "Zadej druhou hodnotu: \n";
+    float cislo2;
+    cin >> cislo2;
+
+    cout << "Zadej operator: +, -, *, /: \n";
+    char op;
+label2:
+    cin >> op;
+
+    switch (op)
+    {
+    case '+':
+        cout << cislo1 << " + " << cislo2 << " = " << cislo1 + cislo2 << "\n";
+        break;
+    case '-':
+        cout << cislo1 << " - " << cislo2 << " = " << cislo1 - cislo2 << "\n";
+        break;
+    case '*':
+        cout << cislo1 << " * " << cislo2 << " = " << cislo1 * cislo2 << "\n";
+        break;
+    case '/':
+        if (cislo2 == 0)
+        {
+            cout << "Nelze delit nulou \n";
+            cout << "Zadej cislo znovu \n";
+            goto label2;
+        }
+        else
+        {
+            cout << cislo1 << " / " << cislo2 << " = " << cislo1 / cislo2 << "\n";
+        }
+        break;
+
+    default:
+
+        cout << "Error! neznamm operaci \n";
+        cout << "Zadej znak znovu \n";
+        goto label2;
+
+    }
+
+    cout << "Dalsi vypocet? Pro ano napis a, pro ne napis n: \n";
+    string odpoved;
+label3:
+    cin >> odpoved;
+    if (odpoved == "a")
+    {
+        goto label1;
+    }
+    else if (odpoved != "n" && odpoved != "a")
+    {
+        cout << "Pro ano napis A, pro ne napis N, nic jineho: \n";
+        goto label3;
+    }
+
+}
+
+// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
+// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
+
+// Tipy pro zahájení práce:
+//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
+//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
+//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
+//   4. K zobrazení chyb použijte okno Seznam chyb.
+//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
+//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
+
---
+++ ZPEL_domaci_priprava/Osetreni_vstupu/Osetreni_vstupu.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int soucin(int dolniMez, int horniMez)
+{
+	int soucin = 1;
+
+	if ((dolniMez >= horniMez) || (dolniMez < 0))
+	{
+		return -1;
+	}
+	else
+	{
+		for (int i = dolniMez; i <= horniMez; i++)
+		{
+			soucin *= i;
+		}
+	}
+
+	return soucin;
+
+}
+
+int main()
+{
+
+		int dolniMez = 2;
+		int horniMez = 5;
+		cout << soucin(dolniMez, horniMez);
+		return 0;
+	
+
+	/*
+	int a;
+
+	cout << "Zadej cislo: ";
+
+	cin >> a;
+
+	if (cin.fail()) 
+	{
+		cout << "Zadana hodnota neni typu int";
+	}
+	//*/
+	
+	/*
+	int a;
+
+	cout << "Zadej cislo: ";
+
+	cin >> a;
+	while (cin.fail())
+	{
+		cout << "Zadana hodnota neni typu int!\nZadej cislo: ";
+		cin.clear();
+		cin.ignore(256,'\n');
+		cin.ignore();
+		cin >> a;
+	}
+	//*/
+
+
+}
